1a5458d9dc9addd803dfafe80960d32e3ec8061d
Nhat Doan <nhat.doan@sap.com>
2023-01-27 16:18:06 +0000

    first project created

--- srv/order-service.js
+++
@@ -1,7 +0,0 @@
-module.exports = cds.service.impl(srv => {
-    srv.after('READ', 'Products', each=> {
-        if (each.stock > 20) {
-            each.name += " -- 5% discount"
-        }
-    })
- })
